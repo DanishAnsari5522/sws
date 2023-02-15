2dea6a62e85bd9e57ca5a6ec8691aaae47785fda
DivyaTirk03 <tirkdivya3@gmail.com>
2023-02-15 22:05:57 +0530

    structure projects

---
+++ src/config/theme.js
@@ -0,0 +1,13 @@
+
+const theme = {
+    colors: {
+        primary: '#9d1faa',
+        secondary: '#E21221',
+        accent: '#f1c40f',
+        background: '#e7e7e7',
+        textColor: "#000"
+    },
+};
+
+
+export default theme;
