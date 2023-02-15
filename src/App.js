0efca4f28580cc07031580c514f825673c7566f2
DivyaTirk03 <tirkdivya3@gmail.com>
2023-02-15 22:44:41 +0530

    setup the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from 'react';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Route } from 'react-router-dom';
+import RootNavigation from './navigation/RootNavigation';
+import Home from './screens/Home'
+import ContactUs from './screens/ContactUs'
 
 function App() {
   return (
     <div className="App">
-      <p>Danish Ansari</p>
+      <BrowserRouter>
+        <RootNavigation />
+        <Route path="/" exact><Home /></Route>
+        <Route path="/contactus"><ContactUs /></Route>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,1 +1,35 @@
 //navigation 
+
+import { Navbar, Nav, Container, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import '../css/navigation/RootNavigation.css'
+import { FaFingerprint } from 'react-icons/fa'
+
+
+function RootNavigation() {
+    return (
+        <div>
+            <div>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="#home"><FaFingerprint className="text-white icon" /> DevDanish</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Link to="/" className="nav-item text-secondary">Home</Link>
+                                <Link to="/contactus" className="nav-item text-secondary">services</Link>
+                                <Link to="/products" className="nav-item text-secondary">products</Link>
+                            </Nav>
+                            <Nav>
+                                <Nav.Link eventKey={2} href="#memes"></Nav.Link>
+                                <Button variant="outline-success">Search</Button>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        </div>
+    )
+}
+
+export default RootNavigation
